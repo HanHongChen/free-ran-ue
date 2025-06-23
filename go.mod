7074daf0de1d66df2bae5d0371df04bc2181c0a6
Alonza0314 <dufengplus@gmail.com>
2025-06-23 18:26:32 +0000

    feat: gnb cmd

--- go.mod
+++ go.mod
@@ -11,14 +11,17 @@ require (
 	github.com/free5gc/openapi v1.1.0
 	github.com/free5gc/sctp v1.0.1
 	github.com/go-playground/assert/v2 v2.2.0
+	github.com/spf13/cobra v1.8.1
 	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
 	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/tim-ywliu/nested-logrus-formatter v1.3.2 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,37 +1,7 @@
 package main
 
-import (
-	"os"
-	"os/signal"
-	"syscall"
-
-	"github.com/Alonza0314/free-ran-ue/gnb"
-	"github.com/Alonza0314/free-ran-ue/logger"
-	"github.com/Alonza0314/free-ran-ue/model"
-	"github.com/Alonza0314/free-ran-ue/util"
-)
+import "github.com/Alonza0314/free-ran-ue/cmd"
 
 func main() {
-	gnbConfig := model.GnbConfig{}
-	if err := util.LoadFromYaml("config/gnb.yaml", &gnbConfig); err != nil {
-		panic(err)
-	}
-
-	logger, err := logger.NewLogger(gnbConfig.Logger.Level)
-	if err != nil {
-		panic(err)
-	}
-
-	gnb := gnb.NewGnb(&gnbConfig, logger)
-	if gnb == nil {
-		return
-	}
-	if err := gnb.Start(); err != nil {
-		return
-	}
-	defer gnb.Stop()
-
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
-	<-sigCh
+	cmd.Execute()
 }

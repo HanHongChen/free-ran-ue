f3eef8e3e89a34451eaf7466436187a50cff7a52
Alonza0314 <alonza.tu@gmail.com>
2025-09-30 18:08:14 +0000

    feat: tcp dial with optional local address

---
+++ ue/tcp_test.go
@@ -0,0 +1,55 @@
+package ue
+
+import (
+	"errors"
+	"net"
+	"testing"
+)
+
+var testTcpDialWithOptionalLocalAddressCases = []struct {
+	name          string
+	remoteAddress string
+	remotePort    int
+	localAddress  string
+}{
+	{
+		name:          "testTcpDialWithoutLocalAddress",
+		remoteAddress: "127.0.0.1",
+		remotePort:    8080,
+		localAddress:  "",
+	},
+	{
+		name:          "testTcpDialWithLocalAddress",
+		remoteAddress: "127.0.0.1",
+		remotePort:    8080,
+		localAddress:  "127.0.0.1",
+	},
+}
+
+func TestTcpDialWithOptionalLocalAddress(t *testing.T) {
+	listener, err := net.Listen("tcp", "127.0.0.1:8080")
+	if err != nil {
+		t.Fatalf("error listening: %v", err)
+	}
+	defer listener.Close()
+
+	go func() {
+		conn, err := listener.Accept()
+		if err != nil {
+			if errors.Is(err, net.ErrClosed) {
+				return
+			}
+			t.Fatalf("error accepting: %v", err)
+		}
+		defer conn.Close()
+	}()
+
+	for _, testCase := range testTcpDialWithOptionalLocalAddressCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			_, err := tcpDialWithOptionalLocalAddress(testCase.remoteAddress, testCase.remotePort, testCase.localAddress)
+			if err != nil {
+				t.Fatalf("error dialing: %v", err)
+			}
+		})
+	}
+}

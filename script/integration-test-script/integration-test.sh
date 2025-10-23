a3df372dfdedcf9e7f5d4538185c0d961c440e81
Alonza0314 <alonza.tu@gmail.com>
2025-10-23 14:26:08 +0000

    feat: dynamic dc test

--- script/integration-test-script/integration-test.sh
+++ script/integration-test-script/integration-test.sh
@@ -23,6 +23,10 @@ FREE5GC_CONSOLE_BASE_URL='http://127.0.0.1:5000'
 FREE5GC_CONSOLE_LOGIN_DATA_FILE="${SCRIPT_DIR}/free5gc-console-login-data.json"
 FREE5GC_CONSOLE_SUBSCRIBER_DATA_FILE="${SCRIPT_DIR}/free5gc-console-subscriber-data.json"
 
+FRU_CONSOLE_BASE_URL='http://127.0.0.1:40104'
+
+FRU_CONSOLE_LOGIN_DATA_FILE="${SCRIPT_DIR}/fru-console-login-data.json"
+
 TEST_POOL="basic|dc-static|dc-dynamic|ulcl"
 
 Usage() {
@@ -90,6 +94,33 @@ free5gc_console_subscriber_action() {
             fi
         ;;
     esac
+}
+
+fru_console_login() {
+    local token=$(curl -s -X POST $FRU_CONSOLE_BASE_URL/api/console/login -H "Content-Type: application/json" -d @$FRU_CONSOLE_LOGIN_DATA_FILE | jq -r '.token' | xargs)
+    if [ -z "$token" ] || [ "$token" = "null" ]; then
+        echo "Failed to get token!"
+        return 1
+    fi
+
+    echo "$token"
+    return 0
+}
+
+fru_console_ue_dc_switch() {
+    local token=$(fru_console_login)
+    if [ -z "$token" ]; then
+        echo "Failed to get token!"
+        return 1
+    fi
+
+    if curl -s --fail -X POST $FRU_CONSOLE_BASE_URL/api/console/gnb/ue/nrdc -H "Content-Type: application/json" -H "Authorization: $token" -d @$FRU_CONSOLE_UE_DC_SWITCH_DATA_FILE; then
+        echo "UE DC switch successful!"
+        return 0
+    else
+        echo "Failed to switch UE DC!"
+        return 1
+    fi
 }
 
 main() {
@@ -176,6 +207,91 @@ main() {
             fi
         ;;
         "dc-dynamic")
+            if ! start_docker_compose $DC_DYNAMIC_COMPOSE_FILE; then
+                echo "Failed to start docker compose!"
+                exit 1
+            fi
+
+            if ! free5gc_console_subscriber_action "post"; then
+                echo "Failed to create subscriber!"
+                stop_docker_compose $DC_DYNAMIC_COMPOSE_FILE
+                exit 1
+            fi
+
+            docker exec -d ue ./free-ran-ue ue -c uecfg.yaml
+
+            sleep 3
+
+            if ! docker exec ue ping -I ueTun0 8.8.8.8 -c 5; then
+                echo "Failed to ping 8.8.8.8!"
+                free5gc_console_subscriber_action "delete"
+                stop_docker_compose $DC_DYNAMIC_COMPOSE_FILE
+                exit 1
+            fi
+
+            if ! docker exec ue ping -I ueTun0 1.1.1.1 -c 5; then
+                echo "Failed to ping 1.1.1.1!"
+                free5gc_console_subscriber_action "delete"
+                stop_docker_compose $DC_DYNAMIC_COMPOSE_FILE
+                exit 1
+            fi
+
+            if ! fru_console_ue_dc_switch; then
+                echo "Failed to switch UE DC!"
+                free5gc_console_subscriber_action "delete"
+                stop_docker_compose $DC_DYNAMIC_COMPOSE_FILE
+                exit 1
+            fi
+
+            sleep 3
+
+            if ! docker exec ue ping -I ueTun0 8.8.8.8 -c 5; then
+                echo "Failed to ping 8.8.8.8!"
+                free5gc_console_subscriber_action "delete"
+                stop_docker_compose $DC_DYNAMIC_COMPOSE_FILE
+                exit 1
+            fi
+
+            if ! docker exec ue ping -I ueTun0 1.1.1.1 -c 5; then
+                echo "Failed to ping 1.1.1.1!"
+                free5gc_console_subscriber_action "delete"
+                stop_docker_compose $DC_DYNAMIC_COMPOSE_FILE
+                exit 1
+            fi
+
+            if ! fru_console_ue_dc_switch; then
+                echo "Failed to switch UE DC!"
+                free5gc_console_subscriber_action "delete"
+                stop_docker_compose $DC_DYNAMIC_COMPOSE_FILE
+                exit 1
+            fi
+
+            sleep 3
+
+            if ! docker exec ue ping -I ueTun0 8.8.8.8 -c 5; then
+                echo "Failed to ping 8.8.8.8!"
+                free5gc_console_subscriber_action "delete"
+                stop_docker_compose $DC_DYNAMIC_COMPOSE_FILE
+                exit 1
+            fi
+
+            if ! docker exec ue ping -I ueTun0 1.1.1.1 -c 5; then
+                echo "Failed to ping 1.1.1.1!"
+                free5gc_console_subscriber_action "delete"
+                stop_docker_compose $DC_DYNAMIC_COMPOSE_FILE
+                exit 1
+            fi
+
+            if ! free5gc_console_subscriber_action "delete"; then
+                echo "Failed to delete subscriber!"
+                stop_docker_compose $DC_DYNAMIC_COMPOSE_FILE
+                exit 1
+            fi
+
+            if ! stop_docker_compose $DC_DYNAMIC_COMPOSE_FILE; then
+                echo "Failed to stop docker compose!"
+                exit 1
+            fi
         ;;
         "ulcl")
             if ! start_docker_compose $ULCL_COMPOSE_FILE; then
